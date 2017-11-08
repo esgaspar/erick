26e64c3ed4007078b3912ec7e8b9d7987f67b6de
Erick S. Gaspar <esgaspar@msn.com>
2017-11-07 23:17:40 -0200

    Chart fix

--- js/script.js
+++ js/script.js
@@ -2,44 +2,47 @@ google.charts.load('current', {packages: ['corechart', 'bar']});
 google.charts.setOnLoadCallback(drawAnnotations);
 
 function drawAnnotations() {
-      var data = google.visualization.arrayToDataTable([
-        ['Habilidade', 'Nível'],
-        ['Java', 5],
-        ['JEE', 4],
-        ['Hibernate', 4],
-        ['EJB', 4],
-        ['Spring', 4.5],
-        ['Java Script', 4],
-        ['HTML5', 4],
-        ['CSS3', 4],
-        ['JSF', 4],
-        ['Struts', 3],
-        ['Jasper', 4],
-        ['Angular JS', 3],
-        ['Node JS', 3],
-        ['Electron', 3],
-        ['SQL', 4],
-        ['Oracle', 3],
-        ['MySql', 3],
-        ['SqlServer', 3],
-        ['Jboss 7', 3],
-        ['Websphere', 3],
-        ['JUnit', 4],
-        ['Mokito', 4],
-        ['Web Services', 4],
-        ['Rest', 4],
-        ['Soap', 3]
+  var data = google.visualization.arrayToDataTable([
+         ['Habilidade', 'Nível', { role: 'annotation' } ],
+         ['Java', 5, 'Java' ],
+         ['JEE', 4, 'JEE' ],
+         ['Hibernate', 4, 'Hibernate' ],
+         ['EJB', 4, 'EJB' ],
+         ['JSF', 4, 'JSF' ],
+         ['Primefaces', 4, 'Primefaces' ],
+         ['Web Services', 4, 'Web Services' ],
+         ['Rest', 4, 'Rest' ],
+         ['Soap', 3, 'Soap' ],
+         ['SQL', 4, 'SQL' ],
+         ['Oracle', 3, 'Oracle' ],
+         ['MySql', 3, 'MySql' ],
+         ['MS Sql Server', 3, 'MS Sql Server' ],
+         ['Jboss', 3, 'Jboss' ],
+         ['TomCat', 3, 'TomCat' ],
+         ['Websphere', 3, 'Websphere' ],
+         ['Spring', 4.5, 'Spring' ],
+         ['Spring MVC', 4, 'Spring MVC' ],
+         ['Spring Batch', 4, 'Spring Batch' ],
+         ['Spring Boot', 3, 'Spring Boot' ],
+         ['Struts', 3, 'Struts' ],
+         ['HTML5', 4, 'HTML5' ],
+         ['CSS3', 4, 'CSS3' ],
+         ['Java Script', 4, 'Java Script' ],
+         ['Jquery', 4, 'Jquery' ],
+         ['Bootstrap', 4, 'Bootstrap' ],
+         ['Node JS', 2, 'Node JS' ],
+         ['Angular JS', 2, 'Angular JS' ],
+         ['Electron', 3, 'Electron' ],
       ]);
 
       var options = {
         title: 'Habilidades', vAxis: {title: 'Nível'},
         title: 'Habilidades',
-        chartArea: {width: '85%'},
+        chartArea: {width: '100%'},
         annotations: {
           alwaysOutside: false,
           textStyle: {
             fontSize: 15,
-            auraColor: 'red',
             color: '#555'
           },
           boxStyle: {
@@ -59,32 +62,3 @@ function drawAnnotations() {
 $(document).ready(function(){
     $('#myFixed').scrollToFixed();
 });
-
-  //  $(function () {
-  //      //incluso essa variavel para setar atributos do css depois
-  //      var elemento = $('.myFixed');
-
-  //      $(window).scroll(function () {
-  //          //distancia que o scroll devera rolar para aparecer o box da div
-  //          if ($(this).scrollTop() > 200) {
-  //              //bloco incluso para setar o css
-  //              elemento.css({
-  //                  'position': 'fixed',
-  //                      'top': '0%',
-  //                      'width':'97.6%',
-  //                      'height':'100%'
-  //              });
-
-  //              $('.element').fadeIn();
-  //          } else {
-  //             //bloco incluso para setar o css
-  //              elemento.css({
-  //                  'position': 'relative',
-  //                      'width':'100%',
-  //                      'height':'100%'
-  //              });
-  //              $('.element').fadeIn();
-  //          }
-  //      });
-  //  });
-    
